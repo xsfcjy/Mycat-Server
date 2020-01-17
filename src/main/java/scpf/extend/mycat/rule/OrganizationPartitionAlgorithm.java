c6a880077eb5d0182fe9097b1a089cbabb564bbf
xsfcjy <xsfcy@126.com>
2020-01-17 17:36:56 +0800

    添加组织分区规则

--- src/main/java/scpf/extend/mycat/rule/OrganizationPartitionAlgorithm.java
+++ src/main/java/scpf/extend/mycat/rule/OrganizationPartitionAlgorithm.java
@@ -1,7 +1,12 @@
 package scpf.extend.mycat.rule;
 
 import com.sun.org.apache.bcel.internal.generic.RETURN;
+import io.mycat.MycatServer;
+import io.mycat.cache.CachePool;
 import io.mycat.route.function.AbstractPartitionAlgorithm;
+import io.mycat.route.sequence.handler.IncrSequenceTimeHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import scpf.extend.mycat.db.jdbc.JdbcHelper;
 
 import java.sql.SQLException;
@@ -9,10 +14,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 根据组织分区规则，如果每次分区字段的数据库索引值有变，会清空SQLRouteCache缓存
+ */
 public class OrganizationPartitionAlgorithm extends AbstractPartitionAlgorithm {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(OrganizationPartitionAlgorithm.class);
+
+
     private static int DEFAULT_DB = 0;
 
-    private Map<String, Integer> partitionCacheMap = new HashMap<>();;
+
+    private CachePool sqlRouteCache;
+
+    private Map<String, Integer> partitionCacheMap = new HashMap<>();
 
     /**
      * 实际分片计算规则的函数
@@ -29,15 +44,57 @@ public class OrganizationPartitionAlgorithm extends AbstractPartitionAlgorithm {
 
     @Override
     public void init() {
-        initialize();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        initialize();
+                        Thread.sleep(10000);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+
     }
+    private boolean hadChangePartition(List list,Map<String,Integer> tempMap){
+        if(list.size() != tempMap.size()){
+            return true;
+        }
+        for(Object o : list){
+            Map map = (Map) o;
+            if(null==tempMap.get(map.get("code"))
+                    || tempMap.get(map.get("code")).intValue() != Integer.valueOf(map.get("partition").toString()).intValue()){
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     public void initialize(){
         try {
-            List list = JdbcHelper.query("SELECT `partition`,`code` FROM scpf_partitions  ");
+            if(null==sqlRouteCache && null!=MycatServer.getInstance()){
+                sqlRouteCache = MycatServer.getInstance().getCacheService().getCachePool("SQLRouteCache");
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append(" SELECT spmd.field_value `code`,spmd.database_index `partition` FROM scpf_partition_field spf ")
+                    .append(" INNER JOIN scpf_partition_match_db spmd ON spmd.partition_field_id = spf.id AND spmd.deleted = 'N' ")
+                    .append(" WHERE  spf.deleted = 'N' AND spf.partition_field = 'zone' ");
+            List list = JdbcHelper.query(sb.toString());
+            Map<String, Integer> tempMap = new HashMap<>();
+            tempMap.putAll(partitionCacheMap);
             for(Object o : list){
                 Map map = (Map) o;
                 partitionCacheMap.put(map.get("code").toString(),Integer.valueOf(map.get("partition").toString()));
+            }
+            if(hadChangePartition(list,tempMap)){
+                sqlRouteCache.clearCache();
+                LOGGER.info("清空SQLRouteCache缓存");
             }
         } catch (SQLException e) {
             e.printStackTrace();
